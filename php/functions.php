15c4ff603fa1db9288bf035dda184ef7ad0a52b1
Jordan Butterwick <33608116+Jordanwbutterwick@users.noreply.github.com>
2019-03-04 08:08:04 -0700

    Update functions.php

--- php/functions.php
+++ php/functions.php
@@ -1,18 +1,17 @@
 <?php
 
-
 function connectDB() {
-    $first_name = $last_name = $email = $birthplace = $phone = ""; 
-    $host="localhost";
-    $user="admin";
-    $pswrd="Butterwick";
-    $dbname="familytree";
+    $first_name = $last_name = $email = $birthplace = $phone = "";
+    $host = "localhost";
+    $user = "admin";
+    $pswrd = "Butterwick";
+    $dbname = "familytree";
 
     $conn = new mysqli($host, $user, $pswrd, $dbname);
-    if ($conn->connect_errno){
+    if ($conn->connect_errno) {
         print("ERROR: There was an error connecting to the database<br>");
-        print("ERROR NUMBER: ". $link->connect_errno . "<br>");
-        print("ERROR MESSAGE: ". $link->connect_error . "<br>");
+        print("ERROR NUMBER: " . $link->connect_errno . "<br>");
+        print("ERROR MESSAGE: " . $link->connect_error . "<br>");
         exit();
     }
     return $conn; //returning the mysqli object
@@ -22,20 +21,22 @@ function closeDB($conn) {
     $conn->close();
 }
 
- function NewPerson() {
+function NewPerson() {
+    if (!isset($_POST)) {
+        exit;
+    }
     $conn = connectDB();
     $newEntry = array("firstname" => $_POST['firstname'],
-    "lastname_maidenname" => $_POST['lastname'], 
-    "birthplace" => $_POST['birthplace'],
-    "email" => $_POST['email'],
-    "phone" => $_POST['phone']);
-
+        "lastname_maidenname" => $_POST['lastname'],
+        "birthplace" => $_POST['birthplace'],
+        "email" => $_POST['email'],
+        "phone" => $_POST['phone']);
 
     $sql = "INSERT INTO person (firstname, lastname_maidenname, birthplace, email, phone) VALUES (?,?,?,?,?)";
-    if($stmt = $conn->prepare($sql)){
+    if ($stmt = $conn->prepare($sql)) {
         // Bind variables to the prepared statement as parameters
         $stmt->bind_param('ssssi', $first_name, $last_name, $birthplace, $email, $phone);
-    
+
         // Set parameters
         $first_name = $_REQUEST['firstname'];
         $last_name = $_REQUEST['lastname'];
@@ -43,24 +44,24 @@ function closeDB($conn) {
         $birthplace = $_REQUEST['birthplace'];
         $phone = $_REQUEST['phone'];
 
-    
-        if ($first_name == "") 
-        { 
-            echo ("First Name cannot be blank"); 
-            exit(); // no need to go further 
-        };
-        if ($last_name == "") 
-        { 
-            echo ("Last Name cannot be blank"); 
-            exit(); // no need to go further 
-        };
+        if ($first_name == "") {
+            echo ("First Name cannot be blank");
+            exit(); // no need to go further
+        }
+        ;
+        if ($last_name == "") {
+            echo ("Last Name cannot be blank");
+            exit(); // no need to go further
+        }
+        ;
 
         // Attempt to execute the prepared statement
-        if($stmt->execute()){
-            echo ($first_name . " has been registered to the database. <a href='index.html'>Return to homepage</a>"); 
-        } else{
+        if ($stmt->execute()) {
+            echo ($first_name . " has been registered to the database. <a href='index.html'>Return to homepage</a>");
+        } else {
             echo "ERROR: Could not execute query: $sql. " . $conn->error;
-        };
+        }
+        ;
         $stmt->close();
         closeDB($conn);
     }
@@ -68,7 +69,7 @@ function closeDB($conn) {
 
 ?>
 
- 
+
 
 
 
